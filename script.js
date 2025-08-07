e935c0d1aaa5b30ee005f23a39868c7992d2e1ef
Kuba-David <jakub.david@hotmail.com>
2025-08-07 15:23:45 +0100

    some pics added

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// Tenhle skript řeší problémy s vh jednotkami. Přepočítává skutečnou výšku viewportu.
-function setRealVH() {
-  const vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--real-vh', `${vh}px`);
-}
-setRealVH();
-window.addEventListener('resize', setRealVH);
+// // Tenhle skript řeší problémy s vh jednotkami. Přepočítává skutečnou výšku viewportu.
+// function setRealVH() {
+//   const vh = window.innerHeight * 0.01;
+//   document.documentElement.style.setProperty('--real-vh', `${vh}px`);
+// }
+// setRealVH();
+// window.addEventListener('resize', setRealVH);
 
 // Tenhle script řeší zalamování jednoznakovek. Přidává nedělitelnou mezeru před písmena, která by mohla zůstat na konci řádku.
 function fixWidows(selector = 'p, h1, h2, h3, h4, span, li') {
